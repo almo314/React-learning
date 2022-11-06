592efae58b23694ecedc17232273568a50063427
almo314 <molokoedov.as@phystech.edu>
2022-11-06 16:07:28 +0300

    add browserRouter

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@ import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
 import Profile from './Components/Profile/Profile';
 import Dialogs from "./Components/Dialogs/Dialogs";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import News from "./Components/News/News";
+import Music from "./Components/Music/Music";
+import Settings from "./Components/Settings/Settings";
 
 function App() {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-        <div className='app-wrapper-content'>
-            <Dialogs />
-        </div>
-      {/*<Profile />*/}
-     </div>
+    <BrowserRouter>
+        <div className='app-wrapper'>
+          <Header />
+          <Navbar />
+            <div className='app-wrapper-content'>
+                <Routes>
+                    <Route path='/dialogs' element={<Dialogs />}/>
+                    <Route path='/profile' element={<Profile />}/>
+                    <Route path='/news' element={<News />}/>
+                    <Route path='/music' element={<Music />}/>
+                    <Route path='/settings' element={<Settings />}/>
+                </Routes>
+            </div>
+         </div>
+    </BrowserRouter>
   );
 };
 
